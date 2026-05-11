d320a009621c55e955f5cbb8e9b0f76bc48a8d1f
takahashiyb <takahashiyb89@gmail.com>
2026-05-12 07:07:26 +0800

    Prepare files for sass

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use '@/assets/styles/variables.scss' as v;
+          @use '@/assets/styles/functions.scss' as f;
+        `,
+      },
     },
   },
 })
