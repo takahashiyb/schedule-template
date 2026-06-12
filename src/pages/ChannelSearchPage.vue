c908bad19594666488160f16c18a00b7b9180f1d
takahashiyb <takahashiyb89@gmail.com>
2026-06-12 15:07:40 +0800

    Refactor pages

---
+++ src/pages/ChannelSearchPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ChannelSearch from '@/components/ChannelSearch.vue'
+</script>
+<template>
+  <Suspense>
+    <ChannelSearch></ChannelSearch>
+  </Suspense>
+</template>
+<style scoped lang="scss"></style>
+
--- src/pages/TemplatePage.vue
+++ src/pages/TemplatePage.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 <template>
-  <RouterView />
+  <Suspense>
+    <RouterView />
+  </Suspense>
 </template>
 <style scoped lang="scss"></style>
