86ffcfa240c89966ba93031c26639664b1739372
takahashiyb <takahashiyb89@gmail.com>
2026-06-25 02:54:47 +0800

    Add live display

--- src/components/talents/phase-connect/DayDisplay.vue
+++ src/components/talents/phase-connect/DayDisplay.vue
@@ -22,14 +22,35 @@ const medium = breakpoints.greater('sm')
 const large = breakpoints.greater('md')
 </script>
 <template>
-  <div class="time" :class="{ medium: medium, large: large }">
+  <div
+    class="time"
+    :class="{
+      medium: medium,
+      large: large,
+      live: props.event.items[0]!.snippet.liveBroadcastContent === 'live',
+    }"
+  >
     <p class="time-display">
-      {{ getShortTime(props.event.items[0]!.liveStreamingDetails.scheduledStartTime, timezone) }}
+      {{
+        props.event.items[0]!.snippet.liveBroadcastContent === 'live'
+          ? 'LIVE'
+          : getShortTime(props.event.items[0]!.liveStreamingDetails.scheduledStartTime, timezone)
+      }}
     </p>
     <p class="title">{{ props.event.items[0]!.snippet.title }}</p>
   </div>
 </template>
 <style scoped lang="scss">
+$lower-left: 10px 10px;
+$left: 20px 0px;
+$upper-left: 30px -10px;
+$upper: 10px -10px;
+$upper-right: -10px -10px;
+$right: -20px 0px;
+$lower-right: -30px 10px;
+$lower: 10px 10px;
+$blur: 25px;
+
 .time {
   --title-bevel: 12px;
   --time-bevel: 3em;
@@ -60,6 +81,39 @@ const large = breakpoints.greater('md')
   box-shadow: 0 -2px 16px 8px hsla(206, 8%, 100%, 0.1);
 }
 
+.time.live .time-display {
+  animation: spinColors 3s linear infinite;
+
+  clip-path: polygon(
+    calc(-12px + var(--time-bevel)) 0,
+    -4px 100%,
+    calc(300% - var(--time-bevel) + 4px) 100%,
+    calc(300% + 4px) 0
+  );
+}
+
+.time.medium.live .time-display {
+  clip-path: polygon(
+    calc(-28px + var(--time-bevel)) 0,
+    -4px 100%,
+    calc(300% - var(--time-bevel) + 4px) 100%,
+    calc(300% + 4px) 0
+  );
+}
+
+.time.live .title {
+  animation: spinColors 3s linear infinite;
+
+  clip-path: polygon(
+    -4px -4px,
+    -4px calc(100% + 4px - var(--title-bevel)),
+    calc(-4px + var(--title-bevel)) calc(100% + 4px),
+    calc(100% + 4px) calc(100% + 4px),
+    calc(100% + 4px) calc(var(--title-bevel) + -4px),
+    calc(100% - var(--title-bevel) + 4px) -4px
+  );
+}
+
 .time.medium .time-display {
   aspect-ratio: 12/1;
 }
@@ -87,5 +141,110 @@ const large = breakpoints.greater('md')
 
 .time:nth-child(odd):last-child .title {
   text-align: center;
+}
+
+@keyframes spinColors {
+  0% {
+    box-shadow:
+      $lower-left $blur red,
+      $left $blur orange,
+      $upper-left $blur yellow,
+      $upper $blur green,
+      $upper-right $blur blue,
+      $right $blur violet,
+      $lower-right $blur indigo,
+      $lower $blur white;
+  }
+  12.5% {
+    box-shadow:
+      $left $blur red,
+      $upper-left $blur orange,
+      $upper $blur yellow,
+      $upper-right $blur green,
+      $right $blur blue,
+      $lower-right $blur violet,
+      $lower $blur indigo,
+      $lower-left $blur white;
+  } /* 360/6 */
+  25% {
+    box-shadow:
+      $upper-left $blur red,
+      $upper $blur orange,
+      $upper-right $blur yellow,
+      $right $blur green,
+      $lower-right $blur blue,
+      $lower $blur violet,
+      $lower-left $blur indigo,
+      $left $blur white;
+  }
+  37.5% {
+    box-shadow:
+      $upper $blur red,
+      $upper-right $blur orange,
+      $right $blur yellow,
+      $lower-right $blur green,
+      $lower $blur blue,
+      $lower-left $blur violet,
+      $left $blur indigo,
+      $upper-left $blur white;
+  }
+  50% {
+    box-shadow:
+      $upper-right $blur red,
+      $right $blur orange,
+      $lower-right $blur yellow,
+      $lower $blur green,
+      $lower-left $blur blue,
+      $left $blur violet,
+      $upper-left $blur indigo,
+      $upper $blur white;
+  }
+  62.5% {
+    box-shadow:
+      $right $blur red,
+      $lower-right $blur orange,
+      $lower $blur yellow,
+      $lower-left $blur green,
+      $left $blur blue,
+      $upper-left $blur violet,
+      $upper $blur indigo,
+      $upper-right $blur white;
+  }
+
+  75% {
+    box-shadow:
+      $lower-right $blur red,
+      $lower $blur orange,
+      $lower-left $blur yellow,
+      $left $blur green,
+      $upper-left $blur blue,
+      $upper $blur violet,
+      $upper-right $blur indigo,
+      $right $blur white;
+  }
+
+  87.5% {
+    box-shadow:
+      $lower $blur red,
+      $lower-left $blur orange,
+      $left $blur yellow,
+      $upper-left $blur green,
+      $upper $blur blue,
+      $upper-right $blur violet,
+      $right $blur indigo,
+      $lower-right $blur white;
+  }
+
+  100% {
+    box-shadow:
+      $lower-left $blur red,
+      $left $blur orange,
+      $upper-left $blur yellow,
+      $upper $blur green,
+      $upper-right $blur blue,
+      $right $blur violet,
+      $lower-right $blur indigo,
+      $lower $blur white;
+  }
 }
 </style>
