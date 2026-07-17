ef9969af2b586d2323f1540652a06d8e3dc57bd2
takahashiyb <takahashiyb89@gmail.com>
2026-07-17 18:55:37 +0800

    Create and setup mid border component for theming and style pippa theme

---
+++ src/components/talents/phase-connect/MidBorder.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="border">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.border {
+  --skew: 35deg;
+  --border: 0, 0%, 100%;
+
+  display: none;
+
+  grid-column: 2/5;
+  grid-row: 2/3;
+
+  z-index: -1;
+  opacity: 0;
+}
+
+.page.large .border {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+
+  opacity: 1;
+}
+
+.border div {
+  width: 70%;
+  background-color: hsl(206, 8%, 15%);
+  border-style: solid;
+  border-color: hsl(var(--border));
+}
+
+.border div:nth-child(1) {
+  grid-column: 2;
+  grid-row: 1;
+
+  transform: skewX(var(--skew));
+  transform-origin: top left; /* pivot point */
+
+  border-width: 4px 4px 0 4px;
+}
+
+.border div:nth-child(2) {
+  grid-column: 2;
+  grid-row: 2;
+
+  transform: skewX(calc(var(--skew) * -1));
+  transform-origin: bottom left; /* pivot point */
+
+  border-width: 0 4px 4px 4px;
+}
+
+.border div:nth-child(3) {
+  grid-column: 1;
+  grid-row: 3;
+  justify-self: end;
+  transform: skewX(calc(var(--skew) * -1));
+
+  transform-origin: top right; /* pivot point */
+
+  border-width: 4px 4px 4px 4px;
+}
+</style>
---
+++ src/components/talents/pipkin-pippa/MidBorder.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="border" style="clip-path: url(#curved)">
+    <div></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.border {
+  --skew: 35deg;
+  --border: 0, 0%, 100%;
+
+  display: grid;
+  grid-template-columns: subgrid;
+  grid-template-rows: repeat(3, 1fr);
+
+  grid-column: 1;
+  grid-row: 3/6;
+
+  z-index: -1;
+}
+
+.page.large .border {
+  grid-column: 2/6;
+  grid-row: 2/3;
+}
+
+.border div:nth-child(1) {
+  background: url('/assets/icons/pipkin-pippa/inconspicuous/png/horizontal.png');
+  background-size: cover;
+  background-position: center;
+
+  grid-column: 1/5;
+  grid-row: 1/4;
+}
+
+.page.large .border div:nth-child(1) {
+  background: url('/assets/icons/pipkin-pippa/inconspicuous-border/png/horizontal.png');
+  background-size: cover;
+  background-position: left top;
+}
+</style>
