79bd8efcce2561863fafc504656d735da37a4252
takahashiyb <takahashiyb89@gmail.com>
2026-07-17 18:56:23 +0800

    Add pippathemed featured art background

---
+++ src/components/talents/pipkin-pippa/FeaturedArt.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { useAnimate } from '@vueuse/core'
+import { ref, useTemplateRef, watch } from 'vue'
+
+const props = defineProps<{ path?: string }>()
+
+const imgSrc = ref(props.path)
+
+function handleError() {
+  imgSrc.value = '/assets/icons/PhaseConnect_Header_Logo.png'
+}
+
+// animation
+const img = useTemplateRef('img')
+
+watch(
+  () => props.path,
+  (newVal) => {
+    imgSrc.value = newVal
+    cancel()
+    play()
+  },
+)
+
+const keyframes = ref([
+  {
+    transform: 'scale(100%) translateY(0%) translateX(0%)',
+    opacity: 1,
+    offset: 0,
+  },
+  {
+    transform: 'scale(100%) translateY(0%) translateX(0%)',
+    opacity: 0,
+    offset: 0.001,
+  },
+  {
+    transform: 'scale(150%) translateY(12.5%) translateX(-100%) ',
+    offset: 0.002,
+  },
+  {
+    transform: 'scale(150%) translateY(12.5%) translateX(0%)',
+    opacity: 1,
+    offset: 0.3,
+  },
+  {
+    transform: 'scale(150%) translateY(12.5%) translateX(20%)',
+    opacity: 0,
+    offset: 0.4,
+  },
+  {
+    transform: 'scale(250%) translateY(-100%) translateX(0%)',
+    opacity: 0,
+    offset: 0.41,
+  },
+  {
+    transform: 'scale(250%) translateY(-100%) translateX(0%)',
+    opacity: 1,
+    offset: 0.42,
+  },
+
+  {
+    transform: 'scale(250%) translateY(15%) translateX(0%)',
+    opacity: 1,
+    offset: 0.8,
+  },
+
+  {
+    transform: 'scale(250%) translateY(15%) translateX(0%)',
+    opacity: 1,
+    offset: 0.803,
+  },
+  {
+    transform: 'scale(250%) translateY(50%) translateX(0%)',
+    opacity: 0,
+    offset: 0.9,
+  },
+  {
+    transform: 'scale(100%) translateY(0%) translateX(0%)',
+    opacity: 0,
+    offset: 0.91,
+  },
+  {
+    opacity: 1,
+  },
+])
+
+const { play, cancel } = useAnimate(img, keyframes, {
+  duration: 6000,
+})
+</script>
+
+<template>
+  <section class="featured-art">
+    <img class="" ref="img" :src="imgSrc" alt="" @error="handleError" />
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use '/src/assets/styles/talents/pipkin-pippa.scss' as s;
+
+.featured-art {
+  --bg-color: hsl(#{s.$flesh});
+  overflow: hidden;
+  height: 100%;
+
+  // background-color: var(--bg-color);
+}
+
+.featured-art img {
+  object-fit: contain;
+  height: 100%;
+  width: 100%;
+
+  z-index: 0;
+}
+</style>
