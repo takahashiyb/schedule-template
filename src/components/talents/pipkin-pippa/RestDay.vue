140855f84ce6525634ae6e60bcfce7dddf4fcbd5
takahashiyb <takahashiyb89@gmail.com>
2026-07-27 09:14:18 +0800

    Add decorations to rest components

--- src/components/talents/pipkin-pippa/RestDay.vue
+++ src/components/talents/pipkin-pippa/RestDay.vue
@@ -2,29 +2,39 @@
 
 <template>
   <div class="rest">
+    <img class="ribbon" src="/assets/icons/pipkin-pippa/Ribbon Collection1.svg" alt="" />
+    <img class="bunny" src="/assets/icons/pipkin-pippa/bunny logo.png" alt="" />
     <p>
       <slot> </slot>
     </p>
+    <img class="bunny" src="/assets/icons/pipkin-pippa/bunny logo.png" alt="" />
+    <img class="ribbon" src="/assets/icons/pipkin-pippa/Ribbon Collection1.svg" alt="" />
   </div>
 </template>
 <style scoped lang="scss">
 @use '/src/assets/styles/talents/pipkin-pippa.scss' as s;
 
 .rest {
+  --border-color: hsl(#{s.$hot-pink});
+
   grid-column: 2/4;
   grid-row: 1;
   justify-self: stretch;
 
+  background-color: hsl(s.$white);
+
+  border: 4px var(--border-color) solid;
+
+  border-radius: 1em;
+
   display: grid;
+  align-items: center;
+  justify-items: center;
   grid-template-columns: repeat(16, 1fr);
 }
 
 p {
-  grid-column: 2/17;
-
-  --border-color: hsl(#{s.$hot-pink});
-
-  background-color: hsl(s.$white);
+  grid-column: 2/16;
   color: hsl(s.$hot-pink);
   min-height: 100%;
   text-align: center;
@@ -32,13 +42,40 @@ p {
   padding-inline: 12px;
   padding-top: 8px;
   padding-bottom: 8px;
-
-  border: 4px var(--border-color) solid;
-
-  border-radius: 1em;
 }
 
 .days.medium .rest p {
+  grid-column: 4/14;
+}
+
+.days.medium .rest {
   border-radius: 9em;
+}
+
+.bunny {
+  display: none;
+  max-height: 3rem;
+  scale: 1.5;
+}
+
+.days.medium .bunny {
+  display: inline-block;
+}
+
+.bunny:nth-child(1) {
+  grid-column: 3;
+}
+
+.ribbon {
+  display: none;
+  max-height: 2rem;
+}
+
+.days.medium .ribbon {
+  display: inline-block;
+}
+
+.ribbon:nth-child(1) {
+  grid-column: 2;
 }
 </style>
